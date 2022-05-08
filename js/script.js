c5fdc96bcb846b01f6a949f586014055db531abc
Patryk Słowiński <patryk_slowinski@o2.pl>
2022-05-08 12:06:27 +0200

    Add Js Game result

--- js/script.js
+++ js/script.js
@@ -27,3 +27,26 @@ if(playerInput == '1'){
 }else if(playerInput == '3'){
     playerMove = 'nożyce';
 }
+printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+}else if(computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrałem!');
+}else if(computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+}else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrałem!');
+}else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+}else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ja wygrałem!');
+}else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+}else if(playerMove == 'nieznany ruch'){
+    printMessage('Nie wybrałeś ruchu, tracisz kolejkę!');
+}	
